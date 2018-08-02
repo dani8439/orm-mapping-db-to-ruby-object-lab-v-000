204c2afbf99a0b3bae85315ca31fbca6c983e3d8
Dani Schuhman <dani8439@aol.com>
2018-08-02 18:15:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,7 +78,10 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SQL
+    DB[:conn].execute(sql)
   end
 
   def self.first_student_in_grade_10
